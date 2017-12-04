6d9439d87c147baa430d90dfec79c88208de08b9
CharlBest <charlbest@yahoo.com>
2017-12-04 12:31:21 +0200

    Add initial tests

---
+++ src/client/app/newsletter/newsletter/newsletter.component.spec.ts
@@ -0,0 +1,36 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { NewsletterComponent } from './newsletter.component';
+import { MatCardModule } from '@angular/material';
+
+describe('NewsletterComponent', () => {
+    let component: NewsletterComponent;
+    let fixture: ComponentFixture<NewsletterComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [NewsletterComponent],
+            imports: [
+                RouterTestingModule,
+                MatCardModule
+            ]
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(NewsletterComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('has page title', async(() => {
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('mat-card-title').textContent).toContain('About us');
+    }));
+});
