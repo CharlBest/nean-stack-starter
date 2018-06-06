6a86b2be360ee9bb2949e4017962a606e709095a
CharlBest <charlbest@yahoo.com>
2018-06-06 07:04:38 +0100

    Add progress spinner global config (brakes animation)

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MAT_PROGRESS_SPINNER_DEFAULT_OPTIONS, MatProgressSpinnerDefaultOptions } from '@angular/material';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -46,7 +47,9 @@ import { WebSocketService } from './shared/websocket.service';
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
       multi: true
-    }
+    },
+    // TODO: This breaks the progress spinner's animation
+    // { provide: MAT_PROGRESS_SPINNER_DEFAULT_OPTIONS, useValue: <MatProgressSpinnerDefaultOptions>{ diameter: 16, strokeWidth: 2 } },
   ],
   bootstrap: [
     AppComponent
