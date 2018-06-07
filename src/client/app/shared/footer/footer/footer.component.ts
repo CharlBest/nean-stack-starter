a98fdd2b8fc4cac7d94f2fd7b0b67fbe33f4a2d3
CharlBest <charlbest@yahoo.com>
2018-06-07 18:49:07 +0100

    Add auto increment package.json (npm) versions + add version number in footer of project

--- src/client/app/shared/footer/footer/footer.component.ts
+++ src/client/app/shared/footer/footer/footer.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
 
+// TODO: Replace require import as soon as Angular supports Typescript 2.9
+// import packageJson from '../../package.json';
+// packageJson.version
+declare function require(moduleName: string): any;
+const { version: appVersion } = require('../../../../../../package.json');
+
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent { }
+export class FooterComponent {
+    appVersion = appVersion;
+}
