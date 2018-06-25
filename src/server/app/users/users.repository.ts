7c133919a1d6ea35fc240db4222114f9dffb2413
CharlBest <charlbest@yahoo.com>
2018-06-25 19:30:36 +0100

    Change repository layer require to imports

--- src/server/app/users/users.repository.ts
+++ src/server/app/users/users.repository.ts
@@ -12,7 +12,7 @@ export class UsersRepository extends BaseRepository {
     }
 
     public async createUser(session: neo4j.Session, uId: string, email: string, username: string, password: string, passwordSalt: string, emailCode: string): Promise<UserModel> {
-        const query = require(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.CreateUser)}`);
+        const query = await import(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.CreateUser)}`);
         const result = await session.run(query.data, { uId, email, username, password, passwordSalt, emailCode });
 
         const model = result.records.map(x => Database.createNodeObject(x.get('user'))) as UserModel[];
@@ -24,7 +24,7 @@ export class UsersRepository extends BaseRepository {
     }
 
     public async doesUsernameAndEmailExist(session: neo4j.Session, email: string, username: string): Promise<DoesUsernameAndEmailExist> {
-        const query = require(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.DoesUsernameAndEmailExist)}`);
+        const query = await import(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.DoesUsernameAndEmailExist)}`);
         const result = await session.run(query.data, { email, username });
 
         const model = result.records.map(x => {
@@ -42,7 +42,7 @@ export class UsersRepository extends BaseRepository {
     }
 
     public async getUser(session: neo4j.Session, emailOrUsername: string): Promise<UserModel> {
-        const query = require(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.GetUser)}`);
+        const query = await import(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.GetUser)}`);
         const result = await session.run(query.data, { emailOrUsername });
 
         const model = result.records.map(x => Database.createNodeObject(x.get('user'))) as UserModel[];
@@ -54,7 +54,7 @@ export class UsersRepository extends BaseRepository {
     }
 
     public async getUserById(session: neo4j.Session, userId: number): Promise<UserModel> {
-        const query = require(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.GetUserById)}`);
+        const query = await import(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.GetUserById)}`);
         const result = await session.run(query.data, { userId });
 
         const model = result.records.map(x => Database.createNodeObject(x.get('user'))) as UserModel[];
@@ -66,7 +66,7 @@ export class UsersRepository extends BaseRepository {
     }
 
     public async forgotPassword(session: neo4j.Session, email: string, code: string): Promise<UserModel> {
-        const query = require(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.AddForgottenPasswordCode)}`);
+        const query = await import(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.AddForgottenPasswordCode)}`);
         const result = await session.run(query.data, { email, code });
 
         const model = result.records.map(x => Database.createNodeObject(x.get('user'))) as UserModel[];
@@ -78,7 +78,7 @@ export class UsersRepository extends BaseRepository {
     }
 
     public async changeForgottenPassword(session: neo4j.Session, email: string, code: string, password: string, passwordSalt: string): Promise<UserModel> {
-        const query = require(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.ChangeForgottenPassword)}`);
+        const query = await import(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.ChangeForgottenPassword)}`);
         const result = await session.run(query.data, { email, code, password, passwordSalt });
 
         const model = result.records.map(x => Database.createNodeObject(x.get('user'))) as UserModel[];
@@ -90,7 +90,7 @@ export class UsersRepository extends BaseRepository {
     }
 
     public async verifyEmail(session: neo4j.Session, userId: number, code: string): Promise<boolean> {
-        const query = require(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.VerifyEmail)}`);
+        const query = await import(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.VerifyEmail)}`);
         const result = await session.run(query.data, { userId, code });
 
         if (result.records.length > 0) {
@@ -101,7 +101,7 @@ export class UsersRepository extends BaseRepository {
     }
 
     public async updateAvatar(session: neo4j.Session, userId: number, avatarUrl: string): Promise<UserModel> {
-        const query = require(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.UpdateAvatar)}`);
+        const query = await import(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.UpdateAvatar)}`);
         const result = await session.run(query.data, { userId, avatarUrl });
 
         const model = result.records.map(x => Database.createNodeObject(x.get('user'))) as UserModel[];
@@ -113,7 +113,7 @@ export class UsersRepository extends BaseRepository {
     }
 
     public async updateBio(session: neo4j.Session, userId: number, bio: string): Promise<UserModel> {
-        const query = require(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.UpdateBio)}`);
+        const query = await import(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.UpdateBio)}`);
         const result = await session.run(query.data, { userId, bio });
 
         const model = result.records.map(x => Database.createNodeObject(x.get('user'))) as UserModel[];
@@ -125,7 +125,7 @@ export class UsersRepository extends BaseRepository {
     }
 
     public async updatePassword(session: neo4j.Session, userId: number, password: string, passwordSalt: string): Promise<UserModel> {
-        const query = require(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.UpdatePassword)}`);
+        const query = await import(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.UpdatePassword)}`);
         const result = await session.run(query.data, { userId, password, passwordSalt });
 
         const model = result.records.map(x => Database.createNodeObject(x.get('user'))) as UserModel[];
@@ -137,7 +137,7 @@ export class UsersRepository extends BaseRepository {
     }
 
     public async deleteUser(session: neo4j.Session, userId: number): Promise<boolean> {
-        const query = require(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.DeleteUser)}`);
+        const query = await import(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.DeleteUser)}`);
         const result = await session.run(query.data, { userId });
 
         if (result.records) {
@@ -148,7 +148,7 @@ export class UsersRepository extends BaseRepository {
     }
 
     public async completedTutorial(session: neo4j.Session, userId: number, viewModel: CompletedTutorial): Promise<boolean> {
-        const query = require(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.CompletedTutorial)}`);
+        const query = await import(`../../core/database/queries/${this.getQueryPath(Folder.Users, Users.CompletedTutorial)}`);
         const result = await session.run(query.data, { userId, tutorialType: viewModel.tutorialType, didSkip: viewModel.didSkip });
 
         if (result.records) {
