be446df3ba17867b9e5e64a50b3867a361736140
CharlBest <charlbest@yahoo.com>
2018-06-25 20:37:13 +0100

    remove set title in server base controller

--- src/server/app/shared/base-controller.ts
+++ src/server/app/shared/base-controller.ts
@@ -2,11 +2,6 @@ import { Request, Response } from 'express';
 
 export abstract class BaseController {
     constructor() { }
-    // TODO: create convienient responses .notFound()... in base controller
-
-    protected async setTitle(response: Response, title: string): Promise<void> {
-        response.locals.title = title;
-    }
 
     protected getUserId(req: Request): number {
         const user = (<any>req).user;
