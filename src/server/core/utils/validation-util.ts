aeef2418d6279f065ba7baada5d4f364da6eb800
CharlBest <charlbest@yahoo.com>
2018-07-03 22:45:54 +0100

    Add server error helpers/util

--- src/server/core/utils/validation-util.ts
+++ src/server/core/utils/validation-util.ts
@@ -1,3 +1,5 @@
+import { Response } from 'express';
+
 export class ValidationUtil {
     static createValidationErrorMessage(key: string, message: string) {
         return {
@@ -14,6 +16,28 @@ export class ValidationUtil {
         return {
             status: 400,
             validation: errors
+        };
+    }
+
+    static addFormError(res: Response, key: string, error: Object) {
+        Object.assign(res.locals.error.formErrors, {
+            [key]: error
+        });
+    }
+
+    static addGlobalError(res: Response, key: string, error: Object) {
+        Object.assign(res.locals.error.globalErrors, {
+            [key]: error
+        });
+    }
+
+    static errorResponse(res: Response) {
+        return {
+            status: 400,
+            validation: {
+                formErrors: res.locals.error.formErrors,
+                globalErrors: res.locals.error.globalErrors
+            }
         };
     }
 }
