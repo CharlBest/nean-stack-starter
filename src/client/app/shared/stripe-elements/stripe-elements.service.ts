7f72db7635441362c9c81bf2c3c2b69744d2c157
CharlBest <charlbest@yahoo.com>
2018-08-09 21:12:05 +0100

    Fix stripe not compiling in AOT

--- src/client/app/shared/stripe-elements/stripe-elements.service.ts
+++ src/client/app/shared/stripe-elements/stripe-elements.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { EventEmitter, Injectable, Output } from '@angular/core';
 import { environment } from '../../../environments/environment';
 
-// declare var Stripe: any;
+declare var Stripe: any;
 
 @Injectable({
     providedIn: 'root'
@@ -16,7 +16,7 @@ export class StripeElementsService {
         this.initializeStripe();
     }
 
-    get stripe(): stripe.Stripe {
+    get stripe() {
         if (!this.stripeInstance) {
             this.stripeInstance = Stripe(environment.stripe.publishableKey);
         }
