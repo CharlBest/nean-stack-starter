ff764161d74a5b823714994522ed7c5cb2fcb56e
CharlBest <charlbest@yahoo.com>
2018-08-13 20:32:29 +0100

    Prevent big upload file (default max size is 10 MB)

--- src/client/app/shared/upload-button/upload-button/upload-button.component.ts
+++ src/client/app/shared/upload-button/upload-button/upload-button.component.ts
@@ -15,13 +15,16 @@ export class UploadButtonComponent {
     @Input() folderName = 'images';
     @Input() showPreview = false;
     @Input() hideProgressBarAfterUpload = true;
+    @Input() maxFileSizeInMB = 10;
     @Output() onUploadComplete: EventEmitter<string> = new EventEmitter();
+    error: string;
 
     constructor(private firebaseStorageService: FirebaseStorageService) { }
 
     handleChange(event: Event) {
         // Reset
         this.previewImgUrl = null;
+        this.error = null;
 
         const file = (<HTMLInputElement>event.target).files[0];
 
@@ -30,9 +33,16 @@ export class UploadButtonComponent {
             return;
         }
 
+        // Max file size
+        if ((file.size / 1024 / 1024 /*in MB*/) > this.maxFileSizeInMB) {
+            this.error = `File exceeds ${this.maxFileSizeInMB} MB. Please upload a smaller file`;
+            return;
+        }
+
         this.firebaseStorageService.upload(file).subscribe(data => {
             this.onUploadComplete.emit(data);
             this.previewImgUrl = data;
+            this.error = null;
         });
 
         this.progressPercentage = this.firebaseStorageService.progress$;
