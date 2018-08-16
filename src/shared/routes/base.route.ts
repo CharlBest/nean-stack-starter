ea63e47bfb14b8be182e7be8161be2575fc0ca5e
CharlBest <charlbest@yahoo.com>
2018-08-16 22:37:46 +0100

    Add ability to a query params to api url in client

--- src/shared/routes/base.route.ts
+++ src/shared/routes/base.route.ts
@@ -1,12 +1,14 @@
 export class BaseRoute {
-    constructor(public rootRoute: string, public route: string, public params?: { [key: string]: any }, public version = 1) { }
+    constructor(public rootRoute: string, public route: string, public params?: { [key: string]: string | number }, public version = 1) { }
 
     server(): string {
         return `/v${this.version}/${this.rootRoute}/${this.route}${this.params ? `/:${Object.keys(this.params).join('/:')}` : ''}`;
     }
 
-    client(): string {
+    client(queryParams?: { [key: string]: string | number }): string {
         let url = `/api/v${this.version}/${this.rootRoute}/${this.route}`;
+
+        // Params
         if (this.params) {
             for (const key in this.params) {
                 if (this.params.hasOwnProperty(key)) {
@@ -14,6 +16,20 @@ export class BaseRoute {
                 }
             }
         }
+
+        // Query params
+        if (queryParams) {
+            let isFirstQueryParam = true;
+            for (const key in queryParams) {
+                if (queryParams.hasOwnProperty(key)) {
+                    if (queryParams[key] !== undefined && queryParams[key] !== null) {
+                        url = `${url}${isFirstQueryParam ? '?' : '&'}${key}=${queryParams[key]}`;
+                        isFirstQueryParam = false;
+                    }
+                }
+            }
+        }
+
         return url;
     }
 }
