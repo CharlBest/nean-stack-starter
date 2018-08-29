62866928a099921be27b8dc8fe4fac8c7d927e18
CharlBest <charlbest@yahoo.com>
2018-08-29 21:52:39 +0100

    Change settings page from button to list + remove cancel butotn on create item page

--- src/client/app/settings/settings.module.ts
+++ src/client/app/settings/settings.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCardModule, MatRadioModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatIconModule, MatListModule, MatRadioModule } from '@angular/material';
 import { LanguageComponent } from './language/language.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { SettingsRoutingModule } from './settings-routing.module';
@@ -9,7 +9,9 @@ import { SettingsComponent } from './settings/settings.component';
 const materialModules = [
   MatButtonModule,
   MatCardModule,
-  MatRadioModule
+  MatRadioModule,
+  MatListModule,
+  MatIconModule,
 ];
 
 @NgModule({
