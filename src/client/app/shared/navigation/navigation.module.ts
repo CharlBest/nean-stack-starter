0e6407c4482928e0bff3fe8199f1898329e0ff1a
CharlBest <charlbest@yahoo.com>
2018-08-29 21:26:20 +0100

    Change bottom toolbar to card for ease of bg color

--- src/client/app/shared/navigation/navigation.module.ts
+++ src/client/app/shared/navigation/navigation.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatBadgeModule, MatButtonModule, MatIconModule, MatSnackBarModule, MatToolbarModule, MatTooltipModule } from '@angular/material';
+import { MatBadgeModule, MatButtonModule, MatCardModule, MatIconModule, MatSnackBarModule, MatToolbarModule, MatTooltipModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
 
@@ -8,6 +8,7 @@ const materialModules = [
   MatIconModule,
   MatButtonModule,
   MatSnackBarModule,
+  MatCardModule,
   MatToolbarModule,
   MatTooltipModule,
   MatBadgeModule,
