e0e5a5c08bb197764efcbc051062b5fcfb8f5042
CharlBest <charlbest@yahoo.com>
2018-09-01 22:00:38 +0100

    Remove unused material imports

--- src/client/app/account/account.module.ts
+++ src/client/app/account/account.module.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCardModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatCardModule, MatIconModule, MatListModule } from '@angular/material';
 import { AccountRoutingModule } from './account-routing.module';
 import { AccountComponent } from './account/account.component';
 
 const materialModules = [
-  MatButtonModule,
   MatCardModule,
   MatListModule,
   MatIconModule,
--- src/client/app/activity/activity.module.ts
+++ src/client/app/activity/activity.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatBadgeModule, MatButtonModule, MatCardModule, MatIconModule, MatListModule, MatTooltipModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatIconModule, MatListModule, MatTooltipModule } from '@angular/material';
 import { ActivityRoutingModule } from './activity-routing.module';
 import { ActivityComponent } from './activity/activity.component';
 
 const materialModules = [
   MatIconModule,
   MatButtonModule,
-  MatBadgeModule,
   MatListModule,
   MatCardModule,
   MatTooltipModule,
