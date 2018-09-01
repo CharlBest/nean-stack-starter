2d37abfa55c21f185a646b1468872fd4aefece34
CharlBest <charlbest@yahoo.com>
2018-09-01 19:18:15 +0100

    Fix font typography not working in custom snackbar for cookie consent

--- src/client/app/shared/cookie-consent/cookie-consent.module.ts
+++ src/client/app/shared/cookie-consent/cookie-consent.module.ts
@@ -4,12 +4,16 @@ import { MatButtonModule, MatSnackBarModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { CookieConsentComponent } from './cookie-consent/cookie-consent.component';
 
+const materialModules = [
+  MatSnackBarModule,
+  MatButtonModule,
+];
+
 @NgModule({
   imports: [
     CommonModule,
-    MatSnackBarModule,
-    MatButtonModule,
     RouterModule,
+    ...materialModules
   ],
   declarations: [
     CookieConsentComponent
