bb858d9406e1412fc204cf7f1e1f6e4170080bc0
CharlBest <charlbest@yahoo.com>
2018-09-03 20:14:59 +0100

    Add a bit more to search page but warn that the feature is not yet implemented

--- src/client/app/search/search.module.ts
+++ src/client/app/search/search.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCardModule, MatInputModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatCardModule, MatInputModule, MatProgressSpinnerModule } from '@angular/material';
+import { FormErrorsModule } from '../shared/form-errors/form-errors.module';
 import { SearchRoutingModule } from './search-routing.module';
 import { SearchComponent } from './search/search.component';
 
@@ -8,12 +10,15 @@ const materialModules = [
   MatButtonModule,
   MatCardModule,
   MatInputModule,
+  MatProgressSpinnerModule,
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     SearchRoutingModule,
+    ReactiveFormsModule,
+    FormErrorsModule,
     ...materialModules
   ],
   declarations: [
--- src/client/app/search/search/search.component.ts
+++ src/client/app/search/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormErrorsService } from '../../shared/form-errors/form-errors.service';
+import { BreakpointService } from '../../shared/services/breakpoint.service';
 
 @Component({
   selector: 'app-search',
@@ -6,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  formGroup: FormGroup;
+  isProcessing = false;
 
-  constructor() { }
+  constructor(public formErrorsService: FormErrorsService,
+    public bpService: BreakpointService,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.formOnInit();
+  }
+
+  formOnInit() {
+    this.formGroup = this.fb.group({
+      term: [null, Validators.required]
+    });
+  }
+
+  onSubmit() {
+    alert('This feature is not yet implemented');
   }
 }
