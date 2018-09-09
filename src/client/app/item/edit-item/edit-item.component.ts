540c22da45941b47ef617aabf7cc37b269f0514e
CharlBest <charlbest@yahoo.com>
2018-09-09 22:51:03 +0100

    only delete media from storage account (Firebase) when user successfully updates their account with the new item media

--- src/client/app/item/edit-item/edit-item.component.ts
+++ src/client/app/item/edit-item/edit-item.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 import { CreateOrUpdateItemViewModel } from '../../../../shared/view-models/item/create-or-update-item.view-model';
 import { ItemViewModel } from '../../../../shared/view-models/item/item.view-model';
 import { FormErrorsService } from '../../shared/form-errors/form-errors.service';
+import { FirebaseStorageService } from '../../shared/services/firebase-storage.service';
 import { ItemFormComponent } from '../item-form/item-form.component';
 import { ItemService } from '../item.service';
 
@@ -18,11 +20,14 @@ export class EditItemComponent implements OnInit {
   itemUId: string;
   isProcessing = true;
   item: ItemViewModel;
+  savedMedia: Array<string>;
+  deleteSubscription: Subscription;
 
   constructor(public formErrorsService: FormErrorsService,
     private itemService: ItemService,
     private router: Router,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private firebaseStorageService: FirebaseStorageService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
@@ -40,6 +45,7 @@ export class EditItemComponent implements OnInit {
       .pipe(finalize(() => this.isProcessing = false))
       .subscribe(data => {
         this.item = data;
+        this.savedMedia = [...data.media];
       }, error => {
         this.formErrorsService.updateFormValidity(error, this.itemForm.formGroup);
       });
@@ -56,9 +62,22 @@ export class EditItemComponent implements OnInit {
     this.itemService.update(this.item.uId, viewModel)
       .pipe(finalize(() => this.itemForm.isProcessing = false))
       .subscribe(data => {
+        this.deleteRemovedImagesFromStorage();
         this.router.navigate(['/item', data.uId]);
       }, error => {
         this.formErrorsService.updateFormValidity(error, this.itemForm.formGroup);
       });
+  }
+
+  deleteRemovedImagesFromStorage() {
+    for (const media of this.savedMedia) {
+      if (!(<Array<string>>this.itemForm.formGroup.get('media').value).includes(media)) {
+        if (this.deleteSubscription) {
+          this.deleteSubscription.unsubscribe();
+        }
+
+        this.deleteSubscription = this.firebaseStorageService.delete(media).subscribe();
+      }
+    }
   }
 }
--- src/client/app/item/item-form/item-form.component.ts
+++ src/client/app/item/item-form/item-form.component.ts
@@ -1,11 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Subscription } from 'rxjs';
 import { BuildFormGroup } from '../../../../shared/validation/validators';
 import { ItemViewModel } from '../../../../shared/view-models/item/item.view-model';
 import { FormErrorsService } from '../../shared/form-errors/form-errors.service';
 import { BreakpointService } from '../../shared/services/breakpoint.service';
-import { FirebaseStorageService } from '../../shared/services/firebase-storage.service';
 
 @Component({
   selector: 'app-item-form',
@@ -18,12 +16,10 @@ export class ItemFormComponent implements OnInit {
   @Input() item: ItemViewModel;
   formGroup: FormGroup;
   isProcessing = false;
-  deleteSubscription: Subscription;
 
   constructor(private fb: FormBuilder,
     public formErrorsService: FormErrorsService,
-    public bpService: BreakpointService,
-    private firebaseStorageService: FirebaseStorageService) { }
+    public bpService: BreakpointService) { }
 
   ngOnInit() {
     this.formOnInit();
@@ -47,12 +43,6 @@ export class ItemFormComponent implements OnInit {
   }
 
   removeMedia(index: number) {
-    const mediaUrl = this.formGroup.get('media').value[index];
-    if (this.deleteSubscription) {
-      this.deleteSubscription.unsubscribe();
-    }
-    this.deleteSubscription = this.firebaseStorageService.delete(mediaUrl).subscribe();
-
     this.formGroup.get('media').value.splice(index, 1);
   }
 }
