6621fcdecaebf9ad5fdff77bb40701ecaeed827f
CharlBest <charlbest@yahoo.com>
2018-09-09 20:33:37 +0100

    Remove item image from firebase storage when it's deleted/removed

--- src/client/app/item/item-form/item-form.component.ts
+++ src/client/app/item/item-form/item-form.component.ts
@@ -1,9 +1,11 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { BuildFormGroup } from '../../../../shared/validation/validators';
 import { ItemViewModel } from '../../../../shared/view-models/item/item.view-model';
 import { FormErrorsService } from '../../shared/form-errors/form-errors.service';
 import { BreakpointService } from '../../shared/services/breakpoint.service';
+import { FirebaseStorageService } from '../../shared/services/firebase-storage.service';
 
 @Component({
   selector: 'app-item-form',
@@ -16,10 +18,12 @@ export class ItemFormComponent implements OnInit {
   @Input() item: ItemViewModel;
   formGroup: FormGroup;
   isProcessing = false;
+  deleteSubscription: Subscription;
 
   constructor(private fb: FormBuilder,
     public formErrorsService: FormErrorsService,
-    public bpService: BreakpointService) { }
+    public bpService: BreakpointService,
+    private firebaseStorageService: FirebaseStorageService) { }
 
   ngOnInit() {
     this.formOnInit();
@@ -43,6 +47,12 @@ export class ItemFormComponent implements OnInit {
   }
 
   removeMedia(index: number) {
+    const mediaUrl = this.formGroup.get('media').value[index];
+    if (this.deleteSubscription) {
+      this.deleteSubscription.unsubscribe();
+    }
+    this.deleteSubscription = this.firebaseStorageService.delete(mediaUrl).subscribe();
+
     this.formGroup.get('media').value.splice(index, 1);
   }
 }
