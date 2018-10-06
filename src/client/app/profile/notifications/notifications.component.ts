3edb2a672e0aaac6c14d14a028b38e6b9d8fc84f
CharlBest <charlbest@yahoo.com>
2018-10-06 12:20:54 +0100

    Confirm dialog before asking for notification permission

--- src/client/app/profile/notifications/notifications.component.ts
+++ src/client/app/profile/notifications/notifications.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSlideToggleChange } from '@angular/material';
+import { DialogService } from '../../shared/dialog/dialog.service';
 import { PushNotificationService } from '../../shared/services/push-notification.service';
 
 @Component({
@@ -12,14 +13,18 @@ export class NotificationsComponent implements OnInit {
   // TODO: not sure if Notification.permission is supported by all browsers
   pushNotificationPermissionGrandted = (<any>Notification).permission === 'granted';
 
-  constructor(private pushNotificationService: PushNotificationService) { }
+  constructor(private pushNotificationService: PushNotificationService,
+    private dialogService: DialogService) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   toggleChange(event: MatSlideToggleChange) {
     if (event.checked) {
-      this.pushNotificationService.subscribeToNotifications();
+      this.dialogService.confirm('Are you sure you would like to receive push notifications?').subscribe(data => {
+        if (data) {
+          this.pushNotificationService.subscribeToNotifications();
+        }
+      });
       // TODO: nothing is being done with this subscription
       // Notification.requestPermission().then((result) => {
       //   if (result === 'denied') {
