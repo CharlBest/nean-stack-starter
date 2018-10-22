76fae11c059a3503bb58ddad82aaa862c942dfb5
CharlBest <charlbest@yahoo.com>
2018-10-22 21:19:48 +0100

    Reference giphy service source code

--- src/client/app/shared/services/giphy.service.ts
+++ src/client/app/shared/services/giphy.service.ts
@@ -1,3 +1,4 @@
+// https://github.com/ndelvalle/ng-giphy/blob/master/src/services/giphy.service.js
 // import { HttpClient } from '@angular/common/http';
 // import { Injectable } from '@angular/core';
 // import { Observable } from 'rxjs';
