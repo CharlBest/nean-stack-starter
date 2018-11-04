c23c51715a776cea6f60a071c58b565334e56949
CharlBest <charlbest@yahoo.com>
2018-11-04 18:37:30 +0000

    Add dev client environment

---
+++ src/client/environments/environment.dev.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  webSocketUrlEndpoint: 'wss://dev.nean.io/',
+  apiUrlEndpoint: 'https://dev.nean.io',
+  googleAnalytics: {
+    web: 'UA-107304143-2',
+    ios: 'UA-102909242-3',
+    chromeExtension: 'UA-102909242-2'
+  },
+  hostUrlForSharingToWeb: 'https://dev.nean.io',
+  firebase: {
+    projectId: 'nean-181415',
+    storageBucket: 'gs://nean-181415.appspot.com/',
+    messagingSenderId: '622256376084'
+  },
+  stripe: {
+    publishableKey: 'pk_live_ee9MNzGQ4YeBswQ2dt8p4e9k'
+  },
+  publicVapidKey: 'BHh0GLT09LUbnWzwX_r3YTmNMeCiDhmCO2KeT35vEcM8CW5DyfP4StRaAWviffMFY7lAK0vLoHl5h1PIubOMp10'
+};
