c830df0cf0506ea46614ca5bba5b2e5ec9ce12fc
CharlBest <charlbest@yahoo.com>
2018-11-06 21:36:38 +0000

    Add timestamp to logger

--- src/server/core/utils/logger.ts
+++ src/server/core/utils/logger.ts
@@ -1,22 +1,25 @@
-import * as winston from 'winston';
+import { createLogger, format, transports } from 'winston';
 import { emailBroker } from '../../communication/emailer-broker';
 
-export const logger = winston.createLogger({
+export const logger = createLogger({
+    format: format.combine(
+        format.timestamp(),
+        format.json()
+    ),
     transports: [
         //
         // In production create file called info.log with error, warn and info level logging in
         // Else create file called debug.log with error, warn, info, verbose and debug level logging in
         //
-        new winston.transports.File({
+        new transports.File({
             filename: process.env.NODE_ENV === 'production' ? 'info.log' : 'src/server/logs/debug.log',
             level: process.env.NODE_ENV === 'production' ? 'info' : 'debug'
         }),
         //
         // Set logging to ouput debug (all) to console if not in prod else only error console logs
         //
-        new (winston.transports.Console)({
-            level: process.env.NODE_ENV === 'production' ? 'error' : 'debug',
-            format: winston.format.simple()
+        new (transports.Console)({
+            level: process.env.NODE_ENV === 'production' ? 'error' : 'debug'
         })
     ]
 });
