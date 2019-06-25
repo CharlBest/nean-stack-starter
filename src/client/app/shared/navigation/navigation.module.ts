1bb68ba0f6402758c6bf5f8221de24501a2381df
CharlBest <charlbest@yahoo.com>
2019-06-25 23:55:47 +0100

    Remove unnecessary Material modules from root (navigation) modules to improve first load perf

--- src/client/app/shared/navigation/navigation.module.ts
+++ src/client/app/shared/navigation/navigation.module.ts
@@ -2,9 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
 import { CookieConsentModule } from '../cookie-consent/cookie-consent.module';
 import { IconsModule } from '../icons/icons.module';
@@ -12,9 +9,6 @@ import { NavigationComponent } from './navigation/navigation.component';
 
 const materialModules = [
   MatButtonModule,
-  MatSnackBarModule,
-  MatToolbarModule,
-  MatTooltipModule,
   MatBadgeModule,
 ];
 
