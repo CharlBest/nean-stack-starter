aad22ebe66c01c7687e908fad38b8597675302de
CharlBest <charlbest@yahoo.com>
2019-07-09 22:22:33 +0100

    Remove hammerjs dependancy

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,7 +1,5 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-// TODO: check if this is still needed. Material issue related to lazy loading hammer js
-import 'hammerjs';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
