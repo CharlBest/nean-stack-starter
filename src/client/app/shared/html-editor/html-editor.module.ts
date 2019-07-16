703af1551519777d28c894372cb745c48a1a7ff7
CharlBest <charlbest@yahoo.com>
2019-07-16 22:22:07 +0100

    Add undo and redo snackbar notifications to html editor

--- src/client/app/shared/html-editor/html-editor.module.ts
+++ src/client/app/shared/html-editor/html-editor.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { IconsModule } from '../icons/icons.module';
 import { HTMLEditorComponent } from './html-editor/html-editor.component';
@@ -9,7 +10,8 @@ import { HTMLEditorComponent } from './html-editor/html-editor.component';
 const materialModules = [
   MatButtonModule,
   MatTooltipModule,
-  MatProgressBarModule
+  MatProgressBarModule,
+  MatSnackBarModule
 ];
 
 @NgModule({
--- src/client/app/shared/html-editor/html-editor/html-editor.component.ts
+++ src/client/app/shared/html-editor/html-editor/html-editor.component.ts
@@ -1,5 +1,6 @@
 // tslint:disable-next-line: max-line-length
-import { AfterViewInit, Component, ElementRef, EventEmitter, Input, Output, SecurityContext, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, HostListener, Input, Output, SecurityContext, ViewChild, ViewEncapsulation } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { DomSanitizer } from '@angular/platform-browser';
 import * as emojione from 'emojione';
 import Quill from 'quill';
@@ -45,7 +46,8 @@ export class HTMLEditorComponent implements AfterViewInit {
     svgIconRedo = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M18.4 10.6C16.55 8.99 14.15 8 11.5 8c-4.65 0-8.58 3.03-9.96 7.22L3.9 16c1.05-3.19 4.05-5.5 7.6-5.5 1.95 0 3.73.72 5.12 1.88L13 16h9V7l-3.6 3.6z"/></svg>';
 
     constructor(private firebaseStorageService: FirebaseStorageService,
-        private domSanitizer: DomSanitizer) { }
+        private domSanitizer: DomSanitizer,
+        private snackBar: MatSnackBar) { }
 
     ngAfterViewInit() {
         this.initQuillEditor();
@@ -252,5 +254,17 @@ export class HTMLEditorComponent implements AfterViewInit {
                 }
             }
         }
+    }
+
+    @HostListener('keydown.control.z') undoSnackBar() {
+        this.snackBar.open('Undo', undefined, {
+            duration: 600
+        });
+    }
+
+    @HostListener('keydown.control.y') redoSnackBar() {
+        this.snackBar.open('Redo', undefined, {
+            duration: 600
+        });
     }
 }
