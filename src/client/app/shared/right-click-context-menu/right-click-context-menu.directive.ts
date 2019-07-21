54ed75c9866bbe07f1a785c21cd9ba0f53716cab
CharlBest <charlbest@yahoo.com>
2019-07-21 18:16:10 +0100

    Only open right click context menu on desktop

--- src/client/app/shared/right-click-context-menu/right-click-context-menu.directive.ts
+++ src/client/app/shared/right-click-context-menu/right-click-context-menu.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, HostListener, Input } from '@angular/core';
 import { ContextMenuComponent } from '../context-menu/context-menu/context-menu.component';
+import { BreakpointService } from '../services/breakpoint.service';
 import { RightClickContextMenuService } from './right-click-context-menu.service';
 
 @Directive({
@@ -9,10 +10,13 @@ export class RightClickContextMenuDirective {
 
     @Input() appRightClickContextMenu: ContextMenuComponent;
 
-    constructor(private rightClickContextMenuService: RightClickContextMenuService) { }
+    constructor(private rightClickContextMenuService: RightClickContextMenuService,
+        private bpService: BreakpointService) { }
 
     @HostListener('contextmenu', ['$event']) onRightClick(event: MouseEvent) {
-        event.preventDefault();
-        this.rightClickContextMenuService.open(event, this.appRightClickContextMenu);
+        if (this.bpService.isDesktop) {
+            event.preventDefault();
+            this.rightClickContextMenuService.open(event, this.appRightClickContextMenu);
+        }
     }
 }
