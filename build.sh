580558b454742cf7c0ea1a9b15b2deb40eea3581
Charl Best <charlbest@yahoo.com>
2019-08-12 21:46:40 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-nv=$(curl -sL https://nodejs.org/download/release/index.tab | awk '($10 != "-" && NR != 1) { print $1; exit}')
-echo "Node LTS" ${nv}
+nodeLTS=$(curl -sL https://nodejs.org/download/release/index.tab | awk '($10 != "-" && NR != 1) { print $1; exit}')
+installedNode=$(node -v)
+echo "Node LTS" ${nodeLTS} " vs Installed ${installedNode}"
 # TODO: fail build if version is to far outdated
 
 # TODO: run 'npm outdated' to check that packages isn't to out of date
