72f723f0c1cbc1586ffc50b854837961ed000366
CharlBest <charlbest@yahoo.com>
2019-09-17 23:01:28 +0100

    Rename test file

--- src/client/test/run.bat
+++ src/client/test/run.bat
@@ -1,3 +1,3 @@
-start cmd /c "node infinite-scroll.js"
-start cmd /c "node infinite-scroll.js"
-start cmd /c "node infinite-scroll.js"
+start cmd /c "node infinite-scroll.spec.js"
+start cmd /c "node infinite-scroll.spec.js"
+start cmd /c "node infinite-scroll.spec.js"
