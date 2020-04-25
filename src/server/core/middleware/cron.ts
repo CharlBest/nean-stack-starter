ae8a45e1a0300493cd3d0a2352022cb69bc93cc7
CharlBest <charlbest@yahoo.com>
2020-04-25 11:58:13 +0100

    Add cron schedules to trigger daily email reports etc.

---
+++ src/server/core/middleware/cron.ts
@@ -0,0 +1,18 @@
+import { CronJob } from 'cron';
+import { emailBroker } from '../../communication/emailer-broker';
+
+class Cron {
+    // Every 5 minutes - check for and send reminders
+    // Every day at 07:15 - email previous day error logs
+
+    // Every day at 07:30 - email previous day system report
+    systemEmailCron = new CronJob('30 07 * * *', () => {
+        emailBroker.system({ success: true });
+    });
+
+    init(): void {
+        this.systemEmailCron.start();
+    }
+}
+
+export const cron = new Cron();
