334b7edf57269e3762eae806b908fed703dcd01f
CharlBest <charlbest@yahoo.com>
2020-04-25 12:00:37 +0100

    Add error handler to server instance

--- src/server/core/server.ts
+++ src/server/core/server.ts
@@ -29,6 +29,27 @@ export class Server {
             this.destroy();
         });
 
+        this.httpServer.on('error', (error: NodeJS.ErrnoException) => {
+            if (error.syscall !== 'listen') {
+                throw error
+            }
+
+            const port = app.get('port');
+            const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+            switch (error.code) {
+                case 'EACCES':
+                    console.error(`${bind} requires elevated privileges`);
+                    process.exit(1);
+                    break;
+                case 'EADDRINUSE':
+                    console.error(`${bind} is already in use`);
+                    process.exit(1);
+                    break;
+                default:
+                    throw error;
+            }
+        });
+
         // TODO: potential events to listen on
         // this.httpServer.on('error', (error) => { });
         // process.on('SIGHUP', () => { });
