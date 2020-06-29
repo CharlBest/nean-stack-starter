ad87d57f7a0712bcc365920725c696074a2c5c0b
CharlBest <charlbest@yahoo.com>
2020-06-29 19:23:04 +0100

    Fix language header

--- src/server/core/middleware/language.ts
+++ src/server/core/middleware/language.ts
@@ -3,7 +3,11 @@ import { NextFunction, Request, Response } from 'express';
 
 export class Language {
     static setLanguage(req: Request, res: Response, next: NextFunction): void {
-        res.locals.language = req.headers['accept-language'] as LanguageType;
+        let language = req.headers['accept-language'];
+        if (language && language.length >= 2) {
+            language = language.substr(0, 2);
+            res.locals.language = language as LanguageType;
+        }
         next();
     }
 }
