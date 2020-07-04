b98b434352bbeea1a38128fc67d91d7b55795ce3
CharlBest <charlbest@yahoo.com>
2020-07-04 19:01:35 +0100

    Fix circular dependancy in cookie consent service

--- src/client/app/shared/cookie-consent/cookie-consent.service.ts
+++ src/client/app/shared/cookie-consent/cookie-consent.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar, MatSnackBarRef } from '@angular/material/snack-bar';
-import { PWAService } from '../pwa-helper/pwa.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { LocalStorageService } from '../services/storage.service';
 import { CookieConsentComponent } from './cookie-consent/cookie-consent.component';
 
@@ -9,31 +8,14 @@ import { CookieConsentComponent } from './cookie-consent/cookie-consent.componen
 })
 export class CookieConsentService {
 
-  snackBarRef: MatSnackBarRef<CookieConsentComponent>;
-  get hasAcceptedCookieConsent(): boolean {
-    return this.localStorageService.storageData.consent;
-  }
-
   constructor(private snackBar: MatSnackBar,
-    private pwaService: PWAService,
     private localStorageService: LocalStorageService) { }
 
   openCookieConsentSnackBar() {
-    if (!this.hasAcceptedCookieConsent) {
-      this.snackBarRef = this.snackBar.openFromComponent(CookieConsentComponent, {
+    if (!this.localStorageService.storageData.consent) {
+      this.snackBar.openFromComponent(CookieConsentComponent, {
         duration: undefined
       });
     }
-  }
-
-  acceptCookieConsent() {
-    this.localStorageService.setUserStorageData({ consent: true });
-
-    if (this.snackBarRef) {
-      this.snackBarRef.dismiss();
-    }
-
-    // Show PWA Install snackbar
-    this.pwaService.openPWAInstallSnackBar();
   }
 }
--- src/client/app/shared/cookie-consent/cookie-consent/cookie-consent.component.ts
+++ src/client/app/shared/cookie-consent/cookie-consent/cookie-consent.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-import { CookieConsentService } from '../cookie-consent.service';
+import { MatSnackBarRef } from '@angular/material/snack-bar';
+import { PWAService } from '../../pwa-helper/pwa.service';
+import { LocalStorageService } from '../../services/storage.service';
 
 @Component({
   templateUrl: './cookie-consent.component.html',
   styleUrls: ['./cookie-consent.component.scss']
 })
 export class CookieConsentComponent {
-  constructor(public cookieConsentService: CookieConsentService) { }
+
+  constructor(private pwaService: PWAService,
+    private localStorageService: LocalStorageService,
+    private snackBarRef: MatSnackBarRef<CookieConsentComponent>) { }
+
+  dismiss() {
+    if (this.snackBarRef) {
+      this.snackBarRef.dismiss();
+    }
+
+    this.localStorageService.setUserStorageData({ consent: true });
+
+    // Show PWA Install snackbar
+    this.pwaService.openPWAInstallSnackBar();
+  }
 }
